db52babc26ae577be7f52784aac719ab992ce240
Maadhih Hussain <maadhihhussain@gmail.com>
2017-02-21 15:39:24 +0500

    Initial commit

---
+++ src/StringRay.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Acnox\StringRay;
+
+/**
+* 
+*/
+
+class String
+{
+	 /**
+     * @var string
+     */
+    protected $string;
+	
+	function __construct()
+	{
+		echo "init";
+	}
+
+	public function test(){
+		echo "test";
+	}
+}

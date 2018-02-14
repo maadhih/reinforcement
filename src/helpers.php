6d39b7a236f75f321b7d7eb95e61a79a06b6e4d6
Maadhih Hussain <maadhih@aasandha.mv>
2018-02-14 19:17:42 +0500

    helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,9 @@
 <?php
 
-function test($string = 'test')
-{
-    return $string;
+if (! function_exists('is_array_assoc')) {
+    function is_array_assoc(array $array)
+    {
+        if (array() === $array) return false;
+	    return array_keys($array) !== range(0, count($array) - 1);
+    }
 }

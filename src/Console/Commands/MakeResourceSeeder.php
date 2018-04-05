8a9c783ddb842185627264cd751ee073bf96680c
Maadhih Hussain <maadhih@aasandha.mv>
2018-04-05 12:09:58 +0500

    update database seeder when making a seeder

--- src/Console/Commands/MakeResourceSeeder.php
+++ src/Console/Commands/MakeResourceSeeder.php
@@ -64,7 +64,10 @@ class MakeResourceSeeder extends AbstractCommand
             $stub = file_get_contents($this->resourcesPath . $ds . 'Stubs' . $ds . (empty($module) ? 'Standard' : 'Modular') . $ds . 'Seeder.stub');
             $seeder = $this->makeSeeder($this->namespace, $resource, $attributes);
 
-            $this->writeFile(Str::plural(ucfirst($resource)) . 'TableSeeder', $seeder);
+            $seederName = Str::plural(ucfirst($resource)) . 'TableSeeder';
+            $this->writeFile($seederName, $seeder);
+            $this->updateDatabaseSeeder($seederName);
+
         }
     }
 
@@ -78,5 +81,15 @@ class MakeResourceSeeder extends AbstractCommand
                 'resourceLower' => str_singular(strtolower($resource)),
                 'attributes' => $attributes,
             ]);
+    }
+
+    public function updateDatabaseSeeder($seederName)
+    {
+        $databaseSeeder = file_get_contents($this->writeDirectory."/DatabaseSeeder.php");
+        $seederCall = "\n".Str::indent(2)."// \$this->call($seederName::class);";
+
+        $newfile = Str::insertAfterLast($databaseSeeder, $seederCall, ');');
+        // dd($newfile);
+        file_put_contents($this->writeDirectory."/DatabaseSeeder.php", $newfile);
     }
 }

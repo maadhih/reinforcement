841466012d995a5dd1ac80a9c2bf4cf1a01f8899
Maadhih Hussain <maadhihhussain@gmail.com>
2018-04-05 12:09:58 +0500

    update database seeder when making a seeder

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -3,10 +3,26 @@
 namespace Reinforcement\Support;
 
 use Illuminate\Support\Str as IlluminateStr;
+use Stringy\Stringy;
 
 class Str extends IlluminateStr
 {
     public static function indent(int $level = 1, string $string = '', int $spaces = 4) {
         return str_repeat(" ", ($level * $spaces)).$string;
+    }
+
+    public static function insertAfter($string, $insert, $after, $last = false)
+    {
+        $string = Stringy::create($string);
+        $index = $last ? $string->indexOfLast($after) : $string->indexOf($after);
+        $firstHalf = $string->substr(0, $index+strlen($after));
+        $secondHalf = $string->substr($index+strlen($after));
+
+        return $firstHalf . $insert . $secondHalf;
+    }
+
+    public static function insertAfterLast($string, $insert, $after)
+    {
+        return static::insertAfter($string, $insert, $after, true);
     }
 }
